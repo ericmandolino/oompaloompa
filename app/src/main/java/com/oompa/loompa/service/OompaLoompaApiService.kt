1d7b38b2b85df06b0d5099d51171dda4f8139d27
Gsus <jejimgo@hotmail.com>
2023-05-26 20:13:49 +0200

    Fixed api service query

--- app/src/main/java/com/oompa/loompa/service/OompaLoompaApiService.kt
+++ app/src/main/java/com/oompa/loompa/service/OompaLoompaApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface OompaLoompaApiService {
-    @GET("napptilus/oompa-loompas?page={page}")
+    @GET("napptilus/oompa-loompas")
     suspend fun getPage(
         @Query("page") page: Int
     ): OompaLoompaPage
