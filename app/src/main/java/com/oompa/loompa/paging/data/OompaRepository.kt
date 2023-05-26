86e33da1c05ed938e497a402be9c6897b659caec
Gsus <jejimgo@hotmail.com>
2023-05-26 18:11:39 +0200

    Added oompa loompa repository

---
+++ app/src/main/java/com/oompa/loompa/paging/data/OompaRepository.kt
@@ -0,0 +1,19 @@
+package com.oompa.loompa.paging.data
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.oompa.loompa.service.OompaLoompaApiService
+import javax.inject.Inject
+
+class OompaRepository @Inject constructor(
+    private val oompaLoompaApiService: OompaLoompaApiService
+) {
+    fun getOompaLoompas() = Pager(
+        config = PagingConfig(
+            pageSize = 20,
+        ),
+        pagingSourceFactory = {
+            OompaPagePagingSource(oompaLoompaApiService)
+        }
+    )
+}
