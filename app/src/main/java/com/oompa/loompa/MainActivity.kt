ff809b14e8231fab9100b316390e1655797eb0d8
Gsus <jejimgo@hotmail.com>
2023-05-26 23:38:22 +0200

    Added initial compose layout

--- app/src/main/java/com/oompa/loompa/MainActivity.kt
+++ app/src/main/java/com/oompa/loompa/MainActivity.kt
@@ -3,15 +3,32 @@ package com.oompa.loompa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.oompa.loompa.ui.theme.LoompaTheme
+import com.oompa.loompa.viewmodel.OompaLoompaViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PagingOompaLoompas()
                 }
             }
         }
@@ -30,17 +47,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun PagingOompaLoompas() {
+    val viewModel = hiltViewModel<OompaLoompaViewModel>()
+    val oompaLoompas = viewModel.getOompaLoompas().collectAsLazyPagingItems()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoompaTheme {
-        Greeting("Android")
+    LazyColumn {
+        items(count = oompaLoompas.itemCount) { index ->
+            val item = oompaLoompas[index]
+            Text(
+                modifier = Modifier.height(96.dp),
+                text = "${item?.first_name} ${item?.last_name}",
+                fontSize = 24.sp
+            )
+            Divider()
+        }
+
+        when (val state = oompaLoompas.loadState.refresh) { //FIRST LOAD
+            is LoadState.Error -> {
+                //TODO Error Item
+                //state.error to get error message
+            }
+
+            is LoadState.Loading -> { // Loading UI
+                item {
+                    Column(
+                        modifier = Modifier.fillParentMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(8.dp),
+                            text = "Refresh Loading" // TODO: strings.xml
+                        )
+
+                        CircularProgressIndicator(color = Color.Black) // TODO: theming
+                    }
+                }
+            }
+
+            else -> {}
+        }
+
+        when (val state = oompaLoompas.loadState.append) { // Pagination
+            is LoadState.Error -> {
+                //TODO Pagination Error Item
+                //state.error to get error message
+            }
+
+            is LoadState.Loading -> { // Pagination Loading UI
+                item {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(text = "Pagination Loading") // TODO: strings.xml
+
+                        CircularProgressIndicator(color = Color.Black) // TODO: theming
+                    }
+                }
+            }
+
+            else -> {}
+        }
     }
 }
