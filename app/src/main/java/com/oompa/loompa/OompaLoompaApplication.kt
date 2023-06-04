fbd51f1bc267da4ff1c4a318dca78332cb2304ef
Gsus <jejimgo@hotmail.com>
2023-06-05 01:20:00 +0200

    Removed unnecessary brackets

--- app/src/main/java/com/oompa/loompa/OompaLoompaApplication.kt
+++ app/src/main/java/com/oompa/loompa/OompaLoompaApplication.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class OompaLoompaApplication : Application() {
-}
+class OompaLoompaApplication : Application()
