f3ebc4001d15ad7d4eccf3974db60f20d1764541
Gsus <jejimgo@hotmail.com>
2023-06-04 11:20:33 +0200

    Display details loading in progress

--- app/src/main/java/com/oompa/loompa/ui/OompaLoompaDetailsScreen.kt
+++ app/src/main/java/com/oompa/loompa/ui/OompaLoompaDetailsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -42,15 +44,11 @@ fun OompaLoompaDetailsScreen(
 ) {
     val oompaLoompa = if (oompaLoompaId != null) detailsScreenViewModel.getOompaLoompa(oompaLoompaId).collectAsState(null).value else null
     val oompaLoompaExtraDetails = if (oompaLoompaId != null) detailsScreenViewModel.getOompaLoompaExtraDetails(oompaLoompaId).collectAsState(null).value else null
-    if (oompaLoompa != null || oompaLoompaExtraDetails != null) {
-        OompaLoompaDetails(
-            oompaLoompa,
-            oompaLoompaExtraDetails,
-            longDetailState = detailsScreenViewModel.getLongDetailState(),
-        )
-    } else {
-        OompaLoompaNoDetails()
-    }
+    OompaLoompaDetails(
+        oompaLoompa,
+        oompaLoompaExtraDetails,
+        longDetailState = detailsScreenViewModel.getLongDetailState(),
+    )
 }
 
 @Composable
@@ -70,27 +68,14 @@ fun OompaLoompaDetails(
                 .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            if (oompaLoompa != null) {
-                OompaLoompaCard(
-                    oompaLoompa = oompaLoompa,
-                    startExpanded = true,
-                    clickable = false,
-                )
-                OompaLoompaFavoriteCard(
-                    oompaLoompaFavorite = oompaLoompa.favorite,
-                    longDetailState,
-                )
-            } else {
-                // TODO: loading state (?)
-            }
-            if (oompaLoompaExtraDetails != null) {
-                OompaLoompaExtraDetailsCard(
-                    oompaLoompaExtraDetails = oompaLoompaExtraDetails,
-                    longDetailState,
-                )
-            } else {
-                // TODO: loading state (?)
-            }
+            MainDetails(
+                oompaLoompa,
+                longDetailState,
+            )
+            ExtraDetails(
+                oompaLoompaExtraDetails,
+                longDetailState,
+            )
         }
         if (longDetailState.showLongDetail) {
             LongDetail(
@@ -103,8 +88,57 @@ fun OompaLoompaDetails(
 }
 
 @Composable
-fun OompaLoompaNoDetails() {
-    Text(text = "No details available :(") // TODO: loading (?) refresh (?)
+fun MainDetails(
+    oompaLoompa: OompaLoompa?,
+    longDetailState: OompaLoompaLongDetailState,
+) {
+    if (oompaLoompa != null) {
+        OompaLoompaCard(
+            oompaLoompa,
+            startExpanded = true,
+            clickable = false,
+        )
+        OompaLoompaFavoriteCard(
+            oompaLoompaFavorite = oompaLoompa.favorite,
+            longDetailState,
+        )
+    } else {
+        OompaLoompaNoDetailsCard()
+        OompaLoompaNoDetailsCard()
+    }
+}
+
+@Composable
+fun ExtraDetails(
+    oompaLoompaExtraDetails: OompaLoompaExtraDetails?,
+    longDetailState: OompaLoompaLongDetailState,
+) {
+    if (oompaLoompaExtraDetails != null) {
+        OompaLoompaExtraDetailsCard(
+            oompaLoompaExtraDetails,
+            longDetailState,
+        )
+    } else {
+        OompaLoompaNoDetailsCard()
+    }
+}
+
+@Composable
+fun OompaLoompaNoDetailsCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(96.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            CircularProgressIndicator()
+        }
+    }
 }
 
 @Composable
@@ -170,7 +204,7 @@ fun LongDetail(
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun LongDetailPreview() {
     LoompaTheme {
@@ -179,5 +213,13 @@ fun LongDetailPreview() {
             detailValue = "Oompa Loompas:\nOompa Loompa doompadee doo\nI've got another puzzle for you\nOompa Loompa doompadah dee\nIf you are wise you'll listen to me\nWhat do you get from a glut of TV?\nA pain in the neck and an IQ of three\nWhy don't you try simply reading a book?\nOr could you just not bear to look?\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no commercials\nOompa Loompa Doompadee Dah\nIf you're like reading you will go far\nYou will live in happiness too\nLike the Oompa\nOompa Loompa doompadee do\nOompa Loompas:\nOompa Loompa doompadee doo\nI've got another puzzle for you\nOompa Loompa doompadah dee\nIf you are wise you'll listen to me\nWhat do you get from a glut of TV?\nA pain in the neck and an IQ of three\nWhy don't you try simply reading a book?\nOr could you just not bear to look?\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no commercials\nOompa Loompa Doompadee Dah\nIf you're like reading you will go far\nYou will live in happiness too\nLike the Oompa\nOompa Loompa doompadee do\nOompa Loompas:\nOompa Loompa doompadee doo\nI've got another puzzle for you\nOompa Loompa doompadah dee\nIf you are wise you'll listen to me\nWhat do you get from a glut of TV?\nA pain in the neck and an IQ of three\nWhy don't you try simply reading a book?\nOr could you just not bear to look?\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no commercials\nOompa Loompa Doompadee Dah\nIf you're like reading you will go far\nYou will live in happiness too\nLike the Oompa\nOompa Loompa doompadee do\nOompa Loompas:\nOompa Loompa doompadee doo\nI've got another puzzle for you\nOompa Loompa doompadah dee\nIf you are wise you'll listen to me\nWhat do you get from a glut of TV?\nA pain in the neck and an IQ of three\nWhy don't you try simply reading a book?\nOr could you just not bear to look?\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no commercials\nOompa Loompa Doompadee Dah\nIf you're like reading you will go far\nYou will live in happiness too\nLike the Oompa\nOompa Loompa doompadee do\nOompa Loompas:\nOompa Loompa doompadee doo\nI've got another puzzle for you\nOompa Loompa doompadah dee\nIf you are wise you'll listen to me\nWhat do you get from a glut of TV?\nA pain in the neck and an IQ of three\nWhy don't you try simply reading a book?\nOr could you just not bear to look?\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no commercials\nOompa Loompa Doompadee Dah\nIf you're like reading you will go far\nYou will live in happiness too\nLike the Oompa\nOompa Loompa doompadee do\nOompa Loompas:\nOompa Loompa doompadee doo\nI've got another puzzle for you\nOompa Loompa doompadah dee\nIf you are wise you'll listen to me\nWhat do you get from a glut of TV?\nA pain in the neck and an IQ of three\nWhy don't you try simply reading a book?\nOr could you just not bear to look?\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no\nYou'll get no commercials\nOompa Loompa Doompadee Dah\nIf you're like reading you will go far\nYou will live in happiness too\nLike the Oompa\nOompa Loompa doompadee do",
             onCloseLongDetailClicked = {},
         )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun OompaLoompaNoDetailsCardPreview() {
+    LoompaTheme {
+        OompaLoompaNoDetailsCard()
     }
 }
